c85700c95b713873edaa9f950ee147e0b8ce35e4
Brandon Lewis <bplewis98@gmail.com>
2021-09-10 14:29:29 -0500

    NA

--- src/Bank/Main.java
+++ src/Bank/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Account acc1 = new Account();
         acc1.getCustomerName();
+        System.out.println("Test");
     }
 }
